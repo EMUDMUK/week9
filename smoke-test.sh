ca92d3571ec1e64dd001792c5fdae1ac7cdcb55f
Mudassir Mukhtar <61210555+EMUDMUK@users.noreply.github.com>
2023-03-30 02:20:55 +0500

    Update smoke-test.sh

--- smoke-test.sh
+++ smoke-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
 
-NODE_IP=$(kubectl get nodes -o jsonpath='{ $.items[0].status.addresses[?(@.type=="ExternalIP")].address }')
-NODE_PORT=$(kubectl get svc calculator-service -o=jsonpath='{.spec.ports[0].nodePort}')
-./gradlew smokeTest -Dcalculator.url=http://${NODE_IP}:${NODE_PORT}
+NODE_IP=$(kubectl get service/calculator-service -o jsonpath='{.spec.clusterIP}')
+NODE_PORT=$(kubectl get svc calculator-service -o=jsonpath='{.spec.ports[0].port}')
+curl http://${NODE_IP}:${NODE_PORT}/sum?a=1\&b=2
