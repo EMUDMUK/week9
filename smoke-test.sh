d0638f018bb39c43aee1996a2cc12d87dac707f6
Mudassir Mukhtar <61210555+EMUDMUK@users.noreply.github.com>
2023-04-10 03:03:23 +0500

    Update smoke-test.sh

--- smoke-test.sh
+++ smoke-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
 
-NODE_IP=$(kubectl get service/calculator-service -o jsonpath='{.spec.clusterIP}')
-NODE_PORT=$(kubectl get svc calculator-service -o=jsonpath='{.spec.ports[0].port}')
-curl http://${NODE_IP}:${NODE_PORT}/sum?a=1\&b=2
+NODE_IP=$(kubectl get service/calculator-service -o jsonpath='{.spec.clusterIP}' -n staging)
+NODE_PORT=$(kubectl get svc calculator-service -o=jsonpath='{.spec.ports[0].port}' -n staging)
+curl http://${NODE_IP}:${NODE_PORT}/sum?a=6\&b=2 -eq 3
